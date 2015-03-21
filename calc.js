8efd3369310abfde6874f0ab120ed60cae0a825c
Ademílson F. Tonato <contato@admcode.com.br>
2015-03-21 15:31:31 -0300

    Adicionado as operações básicas

--- calc.js
+++ calc.js
@@ -1,5 +1,17 @@
+//argumentos do argv são sempre strings
+
 var n1 = process.argv[2]
 ,	op = process.argv[3]
 ,	n2 = process.argv[4];
 
-console.log (n1, op, n2);
+// console.log (n1, op, n2);
+
+if(op === '+') {
+	console.log(Number(n1) + Number(n2));
+} else if(op === '-') {
+	console.log(Number(n1) - Number(n2));
+} else if(op === '*') {
+	console.log(Number(n1) * Number(n2));
+} else if(op === '/') {
+	console.log(Number(n1) / Number(n2));
+}
