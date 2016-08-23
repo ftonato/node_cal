f233c85e7d3df36dfe2bc339def32cbf5b85c17f
Ademílson F. Tonato <contato@admcode.com.br>
2016-08-23 20:11:59 -0300

    Refatorando com Padrões de Projeto

--- calc.js
+++ calc.js
@@ -1,17 +1,25 @@
 //argumentos do argv são sempre strings
 
-var n1 = process.argv[2]
-,	op = process.argv[3]
-,	n2 = process.argv[4];
+let n1 = process.argv[2]
+  ,	op = process.argv[3]
+  ,	n2 = process.argv[4];
 
-// console.log (n1, op, n2);
+//console.log ('_debug: ', n1, op, n2);
 
-if(op === '+') {
-	console.log(Number(n1) + Number(n2));
-} else if(op === '-') {
-	console.log(Number(n1) - Number(n2));
-} else if(op === '*') {
-	console.log(Number(n1) * Number(n2));
-} else if(op === '/') {
-	console.log(Number(n1) / Number(n2));
+switch (op) {
+  case '+':
+    console.log(Number(n1) + Number(n2));
+    break;
+  case '-':
+    console.log(Number(n1) - Number(n2));
+    break;
+  case '*':
+    console.log(Number(n1) * Number(n2));
+    break;
+  case '/':
+    console.log(Number(n1) / Number(n2));
+    break;
+  default:
+    console.log('Operação inválida!')
+    break
 }
